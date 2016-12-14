69c444241fb527a19b196ee98cf279a3f63f3316
Abdón Rodríguez Davila <a@abdonrd.com>
2016-12-14 11:40:03 +0100

    Add specific test config

---
+++ polymer-application-test.js
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright (c) 2016 The IBM Research Emergent Solutions authors. All rights reserved.
+ * This code may only be used under the MIT style license found at https://ibmresearch.github.io/LICENSE.txt
+ * The complete set of authors may be found at https://ibmresearch.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at https://ibmresearch.github.io/CONTRIBUTORS.txt
+ */
+
+'use strict';
+
+module.exports = {
+  extends: 'ibmresearch/polymer-element-test'
+};
